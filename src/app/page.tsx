7827e8274838847b04514c3a4a7d97b25decf37a
allenarduino <aljay3334@gmail.com>
2024-05-18 09:39:40 +0000

    Restyled the main title and sub title on the selectcard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,12 @@ export default function Home() {
     <main>
       <Header />
 
-      <section className=" w-full flex flex-col md:flex md:flex-row md:justify-between items-center px-10 md:mt-10  py-20 py-6 bg-white">
-        <div className="w-full md:w-1/6 md:pl-12   flex justify-start">
-          <button className=" rounded-full px-8 py-2 border-[1px] border-blue-500 font-medium">Back</button>
+      <section className="py-7 w-full text-wrapper flex flex-col justify-center items-center border-red-500 border-2">
+        <div className="border-black-500 border-2 sm:w-full w-full flex sm:items-center sm:justify-center ">
+          <h2 className="font-[400] font-sans text-[25px] font-medium border-blue-500 border-2  leading-loose">What's your goal with Franchain?</h2>
         </div>
-        <div className="w-full text-wrapper flex flex-col items-center">
-          <div className="w-full md:w-1/2 flex justify-start">
-            <h2 className="font-medium font-sans text-4xl  font-medium  leading-loose">What's your goal with Franchain?</h2>
-          </div>
-          <div className="w-full md:w-1/2 justify-start">
-            <p className="text-gray-500 leading-loose">No wrong answers here. And it doesn't matter if you change your mind later.</p>
-          </div>
-
+        <div className="sm:w-1/2 flex justify-center  border-yellow-500 border-2">
+          <p className="text-gray-500 leading-loose border-blue-500 border-2">No wrong answers here. And it doesn't matter if you change your mind later.</p>
         </div>
       </section>
 
