77cca5fe5d2afd0c5bd5bf68e2b4a88f8005a182
allenarduino <aljay3334@gmail.com>
2024-05-18 08:50:53 +0000

    Restyled the select card and the outer grid to be responsive and match the requirements in the UI design

--- src/app/components/SelectCard/SelectCard.tsx
+++ src/app/components/SelectCard/SelectCard.tsx
@@ -6,19 +6,20 @@ interface SelectCardProps { }
 
 const SelectCard: React.FC<SelectCardProps> = () => {
     return (
-        <div className="grid gap-3 place-items-center sm:grid-cols-3 border-gray-500 border-[1px] rounded-[20px] py-5 h-auto">
+        <div className=" flex justify-between sm:grid sm:gap-3 sm:place-items-center grid-cols-2 gap-1 sm:grid-cols-3 border-b sm:border-gray-500 sm:border-[1px] px-2 sm:rounded-[20px] sm:py-5 py-3 h-auto">
             <Image
                 src="/images/funnel-image.png"
                 width="0"
                 height="0"
                 sizes="100vw"
-                className="sm:w-full sm:h-auto h-20 w-20"
+                className="sm:w-full hidden sm:block   sm:h-auto h-20 w-20 "
                 alt="funnel-image"
             />
-            <div className="text-wrapper flex items-center justify-center text-[20px]">Reduce failed payments</div>
-            <div className="flex items-center justify-center">
-                <input type="checkbox" className="h-7 w-7" />
-            </div>
+            <div className="sm:text-wrapper  w-full flex sm:items-center sm:justify-center sm:text-[18px] text-[15px]">Reduce failed payments</div>
+
+            <input type="checkbox" className="sm:h-7 sm:w-7 h-5 w-5 py-2 " />
+
+
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,35 @@ import SelectCard from "./components/SelectCard/SelectCard";
 import Signup from "../app/auth/signup/page";
 
 export default function Home() {
-  /* return (
-     <main>
-       <Header />
- 
-       <section className=" w-full flex flex-col md:flex md:flex-row md:justify-between items-center px-10 md:mt-10  py-20 py-6 bg-white">
-         <div className="w-full md:w-1/6 md:pl-12   flex justify-start">
-           <button className=" rounded-full px-8 py-2 border-[1px] border-blue-500 font-medium">Back</button>
-         </div>
-         <div className="w-full text-wrapper flex flex-col items-center">
-           <div className="w-full md:w-1/2 flex justify-start">
-             <h2 className="font-medium font-sans text-4xl  font-medium  leading-loose">What's your goal with Franchain?</h2>
-           </div>
-           <div className="w-full md:w-1/2 justify-start">
-             <p className="text-gray-500 leading-loose">No wrong answers here. And it doesn't matter if you change your mind later.</p>
-           </div>
- 
-         </div>
-       </section>
- 
-       <section className="md:w-full flex flex-col items-center mb-[150px]">
-         <div className=" container sm:w-[90%] w-[85%] grid md:grid-cols-3  sm:grid-cols-2 grid-cols-1 gap-5">
-           <SelectCard />
-           <SelectCard />
-           <SelectCard />
-           <SelectCard />
-           <SelectCard />
-           <SelectCard />
-         </div>
-       </section>
- 
-     </main>*/
   return (
-    <Signup />
-  );
+    <main>
+      <Header />
+
+      <section className=" w-full flex flex-col md:flex md:flex-row md:justify-between items-center px-10 md:mt-10  py-20 py-6 bg-white">
+        <div className="w-full md:w-1/6 md:pl-12   flex justify-start">
+          <button className=" rounded-full px-8 py-2 border-[1px] border-blue-500 font-medium">Back</button>
+        </div>
+        <div className="w-full text-wrapper flex flex-col items-center">
+          <div className="w-full md:w-1/2 flex justify-start">
+            <h2 className="font-medium font-sans text-4xl  font-medium  leading-loose">What's your goal with Franchain?</h2>
+          </div>
+          <div className="w-full md:w-1/2 justify-start">
+            <p className="text-gray-500 leading-loose">No wrong answers here. And it doesn't matter if you change your mind later.</p>
+          </div>
+
+        </div>
+      </section>
+
+      <section className="sm:w-full flex flex-col items-center mb-[150px] w-full ">
+        <div className=" sm:container w-[100%] sm:w-[90%] w-full sm:w-[85%] grid  lg:grid-cols-3 md:grid-cols-2  sm:grid-cols-2 grid-cols-1 sm:gap-5">
+          <SelectCard />
+          <SelectCard />
+          <SelectCard />
+          <SelectCard />
+          <SelectCard />
+          <SelectCard />
+        </div>
+      </section>
+    </main>
+  )
 }
