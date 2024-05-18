1b844f61f426b07305948c90ea3b7bd67ea87625
allenarduino <aljay3334@gmail.com>
2024-05-18 23:35:50 +0000

    Made the image and the grid container more responsive

--- src/app/description/page.tsx
+++ src/app/description/page.tsx
@@ -7,9 +7,9 @@ const Description: React.FC<DescriptionProps> = () => {
     return (
         <main>
             <section className="flex h-full border-green-500 border-2 flex-col items-center justify-center w-full absolute">
-                <div className="grid sm:grid-cols-2 w-[65%] pl-[15px] max-w-[1300px] h-[500px] border-red-500 border-2">
-                    <div className="text-wrapper  cols-span-3 flex flex-col text-start justify-center border-green-500 border-2">
-                        <p className="font-[400] lg:text-[20px] border-blue-500 border-2 w-[80%]  ">
+                <div className="grid lg:grid-cols-2 grid-cols-1 w-[90%] md:w-[70%] lg:w-[80%] xl:w-[70%] pl-[15px] md:max-w-[1300px]  h-[482px] border-red-500 border-2">
+                    <div className="text-wrapper  sm:cols-span-4 flex flex-col text-start justify-center border-green-500 border-2">
+                        <p className="font-[500] lg:text-[25px] border-blue-500 border-2 w-[95%]  ">
                             Franchain has helped us collect royalties locally and internationally in a way we never thought was possible
                         </p>
 
@@ -19,18 +19,18 @@ const Description: React.FC<DescriptionProps> = () => {
 
 
                     </div>
-                    <div className="border-yellow-500 relative cols-span-1 flex items-center justify-end ">
+                    <div className="border-black-500 border-2 relative sm:h-[500px] xl:h-[520px]  h-[400px] cols-span-1 flex items-center lg:justify-end justify-center ">
                         <Image
                             src="/images/description-image.png"
                             width="0"
                             height="0"
                             sizes="100vw"
                             alt="screen-image"
-                            className="w-[80%] h-[100%] absolute rounded-[12.75px] min-w-[350px]"
-                            objectFit="cover"
+                            className=" absolute lg:w-[80%] rounded-[12.75px]  w-[70%] h-[100%]  min-w-[364px] cols-span-1  "
+
 
                         />
-                        <div className="bg-custom-gradient2 min-w-[350px] rounded-lg z-10 h-[100px] w-[80%] absolute bottom-0 px-[12px] pb-[12px] flex flex-col justify-end">
+                        <div className="bg-custom-gradient2 min-w-[364px] lg:w-[80%] w-[70%] rounded-lg z-10 h-[100px] absolute bottom-0 px-[12px] pb-[12px] flex flex-col justify-end">
                             <p className="text-white font-[500]">Alexandra Miller</p>
                             <p className="text-white font-[500]">Founder @ EL&N London</p>
                         </div>
