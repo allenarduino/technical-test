45f20216dbf57ccf5caf66e580cb3c76fe0d1872
allenarduino <aljay3334@gmail.com>
2024-05-20 23:01:14 +0000

    Added the tailwind css code to make turn the border of a card to blue if it is selected

--- src/app/components/SelectCard/SelectCard.tsx
+++ src/app/components/SelectCard/SelectCard.tsx
@@ -22,7 +22,7 @@ const SelectCard = () => {
         <>
             {
 
-                cards.map(card => <div className=" flex justify-between sm:grid sm:gap-3 sm:place-items-center grid-cols-2 gap-1 sm:grid-cols-3 border-b sm:border-gray-500 sm:border-[1px] px-5 sm:rounded-[20px] sm:py-5 py-3 h-auto">
+                cards.map(card => <div className={`flex justify-between sm:grid sm:gap-3 sm:place-items-center grid-cols-2 gap-1 sm:grid-cols-3 border-b ${card.selected ? 'sm:border-custom-blue' : 'sm:border-gray-300'} sm:border-[1px] px-5 sm:rounded-[20px] sm:py-5 py-3 h-auto`}>
                     <Image
                         src={`${card.image}`}
                         width="0"
@@ -32,7 +32,7 @@ const SelectCard = () => {
                         alt="funnel-image"
                     />
                     <div className="sm:text-wrapper  w-full flex sm:items-center sm:justify-center sm:text-[18px] text-[15px]">{card.text}</div>
-                    <input type="checkbox" checked={card.selected} onChange={() => handleCheckboxChange(card.id)} className="sm:h-7 sm:w-7 h-5 w-5 py-2 " />
+                    <input type="checkbox" checked={card.selected} onChange={() => handleCheckboxChange(card.id)} className="sm:h-7 sm:w-7 h-5 w-5 py-2 cursor-pointer " />
 
 
                 </div>)
