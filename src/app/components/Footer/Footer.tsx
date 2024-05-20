19f79f03a3fc895414078e7991f4fb0513d1adc9
allenarduino <aljay3334@gmail.com>
2024-05-20 13:30:15 +0000

    Adjusted the padding of the footer items to make it meet the requirements

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,18 +6,18 @@ interface FooterProps { }
 const Footer: React.FC<FooterProps> = () => {
     return (
         <footer className=" bottom-0  left-0 h-auto  w-full flex  justify-center items-center flex-col bg-white text-gray-600 py-5 flex flex-wrap justify-between border-t border-gray-200">
-            <div className="text-wrapper container sm:w-[90%] w-[85%]  flex justify-start items-center">
+            <div className="text-wrapper xl:max-w-[1300px] sm:w-[90%] w-[85%]  flex justify-start items-center">
                 <p>Franchain is a financial technology company, not a bank. Banking services are provided by trusted regulated banking partners including but not limited to JP Morgan, Barclays and Credit Suisse.</p>
             </div>
-            <div className=" md:py-8 text-wrapper container sm:w-[90%] w-[85%] flex-col flex justify-start  md:flex-row font-[600]">
+            <div className="xl:max-w-[1300px] md:py-8 text-wrapper  sm:w-[90%] w-[85%] flex-col flex justify-start  md:flex-row font-[600]">
                 <div className="flex">
-                    <a href="#" className="text-gray-600 hover:text-gray-700 px-1 xl:px-12 lg:px-10 py-3 flex  ">Need help?</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-700 px-1 xl:px-12 lg:px-10 py-3 flex ">Back to dashboard</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-700  py-3 flex  ">Need help?</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-700 px-2 xl:px-12 lg:px-10 py-3 flex ">Back to dashboard</a>
                     <a href="#" className="text-gray-600 hover:text-gray-700 px-1 xl:px-12 lg:px-10  py-3 flex ">Terms</a>
                 </div>
                 <div className="flex">
-                    <a href="#" className="text-gray-600 hover:text-gray-700 px-1 xl:px-12 lg:px-10 py-3  flex ">Privacy policy</a>
-                    <a href="#" className="text-gray-600 hover:text-gray-700 px-1 xl:px-12 lg:px-10 py-3  flex ">Sign out and continue later</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-700 px-1 sm:px-1  xl:px-12 lg:px-10 py-3  flex ">Privacy policy</a>
+                    <a href="#" className="text-gray-600 hover:text-gray-700 px-2 sm:px-4 xl:px-12 lg:px-10 py-3  flex ">Sign out and continue later</a>
                 </div>
             </div>
         </footer>
